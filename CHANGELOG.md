f8056ee31ba65362aa3984c45930d44f60481c6b
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-04-14 11:32:39 +0200

    Add base features

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+1.0.0
+=====
+
+*   (feature) Add `CommandHelper`.
+*   (feature) Add `TorrStyle`.
--- src/CliBundle.php
+++ src/CliBundle.php
@@ -3,10 +3,21 @@ declare(strict_types=1);
 
 namespace Torr\Cli;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Torr\BundleHelpers\Bundle\BundleExtension;
 
 final class CliBundle extends Bundle
 {
+	/**
+	 * @inheritDoc
+	 */
+	public function getContainerExtension () : ExtensionInterface
+	{
+		return new BundleExtension($this);
+	}
+
+
 	/**
 	 * @inheritDoc
 	 */
